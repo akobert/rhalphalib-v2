1e99ee67ad84bb2aed78a9b94b12eec8688699c3
Adam Kobert <adam.kobert@physics.rutgers.edu>
2025-05-01 13:35:05 -0400

    Adding 2018 PN90 closure tests

---
+++ Closure_Test_2018_PN90/pass_4bin/2018Fit.py
@@ -0,0 +1,39 @@
+import ROOT
+from ROOT import *
+import numpy
+import math
+
+def main():
+	myFunc = TF1("myFunc", "[0]*(sin([1]*sqrt(x))/x)+[2]", 5, 200)
+	refF = ROOT.TFile.Open("diagnositcs.root", 'read')
+
+	for i in range(0,4):
+		myFunc.SetParameters(2.78,4.64,1)
+#		print(myFunc(12.5))
+
+		hist = refF.Get("2018_data_bkg_ratio_pTbin"+str(i))
+	
+		hist.Fit(myFunc,"","",5,25)
+
+		parameter_0 = myFunc.GetParameter(0)
+		error_0 = myFunc.GetParError(0)
+		parameter_1 = myFunc.GetParameter(1)
+		error_1 = myFunc.GetParError(1)
+		parameter_2 = myFunc.GetParameter(2)
+		error_2 = myFunc.GetParError(2)
+#		parameter_3 = myFunc.GetParameter(3)
+#		error_3 = myFunc.GetParError(3)
+		print(f"Parameter 0: {parameter_0} +/- {error_0}")
+		print(f"Parameter 1: {parameter_1} +/- {error_1}")
+		print(f"Parameter 2: {parameter_2} +/- {error_2}")
+#		print(f"Parameter 3: {parameter_3} +/- {error_3}")
+	
+		c1 = TCanvas()
+		c1.cd()
+		hist.Draw()
+		c1.Print("./fit"+str(i)+".png")
+		c1.Close()
+	
+
+if __name__ == "__main__":
+	main()
---
+++ Closure_Test_2018_PN90/total_5bin/make_multiSig_Plot_new_33.py
@@ -0,0 +1,26 @@
+import ROOT
+from ROOT import *
+import os
+from array import array
+import math
+from math import *
+import sys
+import glob
+import csv
+import ctypes
+from ctypes import *
+import XRootD
+from pyxrootd import client
+import numpy as np
+
+from drawDiagnostics_new_33 import *
+
+#from future import division
+
+if __name__ == "__main__":
+	print("Starting Run")
+	ifile1 = "sig25/fitDiagnosticsTest.root"
+	name = "NEW_multiSig_total_5bin"
+	mass = 25
+	RData = drawDiagnostic(name, ifile1, mass)
+	print("draw Diagnositcs Finished")
